d0dd65b4a4c29e7744ee348a11b1ce40e51ba777
Alex McMaster <mcmaster2010@gmail.com>
2023-03-07 12:02:44 +0100

    check-training: several changes to this script
    * Support chaining multiple log files together (in case of interrupted
      training)
    * Change moving average filter
    * Several minor tweaks and bug fixes

--- check-training.py
+++ check-training.py
@@ -1,6 +1,7 @@
 import sys
 
 import numpy as np
+import scipy.optimize
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
@@ -9,7 +10,17 @@ def moving_average(a, n=3) :
     ret = np.cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     #return ret[n - 1:] / n
-    return ret / n
+    #return np.hstack((a[:n], ret[n:] / n))
+    return ret / np.hstack((np.arange(n)+1, np.full(len(ret)-n, n)))
+
+
+def reject_outliers(data, m = 2.):
+    if isinstance(data, list):
+        data = np.array(data)
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0.
+    return data[s<m]
 
 
 lr_steps = list()
@@ -19,35 +30,71 @@ train_loss = list()
 dev_loss_steps = list()
 dev_loss = list()
 
-for summary in tf.compat.v1.train.summary_iterator(sys.argv[1]):
-    s = summary.summary.value
-    if len(s) < 1:
-        print("Empty event:")
-        print(summary)
-        continue
-    tag = s[0].tag
-    val = s[0].simple_value
-    if tag == "train/loss":
-        train_loss_steps.append(summary.step)
-        train_loss.append(val)
-    elif tag == "lr":
-        lr_steps.append(summary.step)
-        lr.append(val)
-    elif tag == "dev/loss":
-        dev_loss_steps.append(summary.step)
-        dev_loss.append(val)
-    else:
-        print(f"Unrecognized tag: {tag}")
+for i, logfile in enumerate(sys.argv[1:]):
+    lr_step_offset = 0
+    train_loss_step_offset = 0
+    dev_loss_step_offset = 0
+    for summary in tf.compat.v1.train.summary_iterator(logfile):
+        s = summary.summary.value
+        if len(s) < 1:
+            print("Empty event:")
+            print(summary)
+            continue
+        tag = s[0].tag
+        val = s[0].simple_value
+        if tag == "train/loss":
+            if i > 0 and train_loss_step_offset == 0:
+                train_loss_step_offset = train_loss_steps[-2] - summary.step
+            train_loss_steps.append(summary.step + train_loss_step_offset)
+            train_loss.append(val)
+        elif tag == "lr":
+            if i > 0 and lr_step_offset == 0:
+                lr_step_offset = lr_steps[-2] - summary.step
+            lr_steps.append(summary.step + lr_step_offset)
+            lr.append(val)
+        elif tag == "dev/loss":
+            if i > 0 and dev_loss_step_offset == 0:
+                dev_loss_step_offset = dev_loss_steps[-2] - summary.step
+            dev_loss_steps.append(summary.step + dev_loss_step_offset)
+            dev_loss.append(val)
+        else:
+            print(f"Unrecognized tag: {tag}")
 
-ma_n = 200
-train_loss_ma = moving_average(train_loss, ma_n)
+
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+
 ax1.plot(lr_steps, lr)
+
+ax1.set_title("MotionCNN Training Progress")
 ax1.set_ylabel("Learning Rate")
-ax2.plot(train_loss_steps, moving_average(train_loss, ma_n))
+
+legend = list()
+ax2.plot(train_loss_steps, train_loss)
+legend.append("Training Loss")
+try:
+    ma_n = 200
+    train_loss_ma = moving_average(train_loss, ma_n)
+    ax2.plot(train_loss_steps, train_loss_ma)
+    legend.append(f"Training Loss (MA{10*ma_n})")
+except Exception as err:
+    print(f"Failed to plot train_loss_ma: {err}")
 ax2.plot(dev_loss_steps, dev_loss)
+legend.append("Validation Loss")
+try:
+    dev_loss_best = [min(dev_loss[:i+1]) for i in range(len(dev_loss))]
+    # ^^ Painfully inefficient, but still fast at the scale we're dealing with.
+    ax2.plot(dev_loss_steps, dev_loss_best)
+    legend.append("Best Validation Loss")
+except Exception as err:
+    print(f"Failed to plot dev_loss_best: {err}")
+
 ax2.set_xlabel("Training Step")
 ax2.set_ylabel("Loss")
-ax2.set_ylim(0, 1.1 * train_loss_ma[ma_n-1])
-ax2.legend([f"Training Loss (MA{10*ma_n})", "Validation Loss"])
+try:
+    ax2.set_ylim(0, 1.5 * max(reject_outliers(dev_loss)))
+except Exception as err:
+    print(f"Failed to set ylim: {err}")
+
+ax2.legend(legend)
+
 plt.show()
